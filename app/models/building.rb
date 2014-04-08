428b9b595434efcfb4a5d391979785b4377e69d9
Adam Sheehan <a.t.sheehan@gmail.com>
2014-04-08 14:11:08 -0400

    Test building validations.

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,6 +1,18 @@
 class Building < ActiveRecord::Base
+
+  def self.states
+    [
+      "AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL",
+      "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA",
+      "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE",
+      "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI",
+      "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"
+    ]
+  end
+
   validates :street_address, presence: true
   validates :city, presence: true
-  validates :state, presence: true
+  validates :state, presence: true, inclusion: { in: Building.states }
   validates :postal_code, presence: true
+
 end
